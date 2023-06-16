c1d9fde9eec88b2f8859a01676ca33d07d835ef0
Shi Hao Hong <shihaohong94@gmail.com>
2023-06-16 15:23:28 +0800

    2-1

---
+++ learning_c/ch1/README.md
@@ -0,0 +1,6 @@
+# Naming convention of dirs
+
+Originally wanted to name things by their intended use case. Realized it'll
+be hard to track/trace if I ever want to reference it to classmates and for
+my own personal use. Subsequent sections will be explicitly named by
+the exercise names.
---
+++ learning_c/ch2/2-1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <limits.h>
+
+// had to look up the source code for limits.h
+// source code link: https://www.gnu.org/software/libc/sources.html
+int main() {
+    // char
+    // %d is for signed decimal int
+    printf("char lowest val: %d\n", CHAR_MIN);
+    printf("char lowest val: %d\n", CHAR_MAX);
+    printf("unsigned char range: %d\n", UCHAR_MAX);
+
+    // short
+    // %hi for signed short
+    // %hu for unsigned short
+    printf("short lowest val: %hi\n", SHRT_MIN);
+    printf("short lowest val: %hi\n", SHRT_MAX);
+    printf("unsigned short range: %hu\n", USHRT_MAX);
+
+    // int
+    // %i for signed int. %i will take base value as entered.
+    // %u for unsigned int
+    printf("int lowest val: %i\n", INT_MIN);
+    printf("int lowest val: %i\n", INT_MAX);
+    printf("unsigned int range: %u\n", UINT_MAX);
+
+    // long
+    // %ld for long
+    // %lu is for unsigned long values
+    printf("long lowest val: %ld\n", LONG_MIN);
+    printf("long lowest val: %ld\n", LONG_MAX);
+    printf("unsigned long range: %lu\n", ULONG_MAX);
+}
