d10fb94923436d5235f838c549a403e1cb15f68c
Shi Hao Hong <shihaohong94@gmail.com>
2023-06-18 20:23:12 +0800

    remove static for init

--- prep_phase/learning_c/ch4/4-11.c
+++ prep_phase/learning_c/ch4/4-11.c
@@ -18,21 +18,24 @@ int main(){
     char ca[MAXLENGTH];
     int i2 = 0;
     char ca2[MAXLENGTH];
-    // gets only one operator/number
+    int i3 = 0;
+    char ca3[MAXLENGTH];
     i = getop(ca);
-    printf("resulting val: %d\n", i);
+    printf("resulting ascii int val: %d\n", i);
     printf("resulting string: %s\n", ca);
     i2 = getop(ca2);
-    // print the int value of the next char
-    printf("resulting val2: %d\n", i2);
+    printf("resulting ascii int val: %d\n", i2);
     printf("resulting string2: %s\n", ca2);
+    i3 = getop(ca3);
+    printf("resulting ascii int val3: %d\n", i3);
+    printf("resulting string3: %s\n", ca3);
 }
 
 /* getop: get next operator or numeric operand */
 int getop(char s[])
 {
     int i;
-    static char c;
+    char c;
 
     while ((s[0] = c = getch()) == ' ' || c == '\t')
         ;
