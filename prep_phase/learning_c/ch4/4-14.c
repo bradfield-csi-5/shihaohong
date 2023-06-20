54d6e5ed6a515f5b8846f61cb07cbf78da5888dd
Shi Hao Hong <shihaohong94@gmail.com>
2023-06-20 08:50:38 +0800

    some experimenting after some ch5 reading

--- prep_phase/learning_c/ch4/4-14.c
+++ prep_phase/learning_c/ch4/4-14.c
@@ -3,6 +3,8 @@
 // swap fn cannot be declared after #define, else it will
 // be replaced by the macro and the program won't compile.
 void swap(int x, int y);
+void swap_ptr(int* x, int* y);
+
 #define swap(t,x,y) { t tmp; tmp = x; x = y; y = tmp; }
 
 int main() {
@@ -20,16 +22,30 @@ int main() {
     a = 1;
     b = 2;
     #undef swap
-    printf("==== USING A FN ====\n");
+    printf("==== USING A FN (as expected, doesnt work)====\n");
     printf("before --> a: %d, b: %d\n", a, b);
     swap(a, b);
     printf("after --> a: %d, b: %d\n", a, b);
 
+    printf("==== USING PTR FN (as expected, works)====\n");
+    printf("before --> a: %d, b: %d\n", a, b);
+    swap_ptr(&a, &b);
+    printf("after --> a: %d, b: %d\n", a, b);
 }
 
+// doesnt work because the variables are copied (pass by value)
 void swap(int x, int y) {
     int tmp;
     tmp = x;
     x = y;
     y = tmp;
+}
+
+// works because fn takes pointers and this fn updates
+// the values in each address directly
+// x and y are pointer copies
+void swap_ptr(int* x, int* y) {
+    int tmp = *x;
+    *x = *y;
+    *y = tmp;
 }
