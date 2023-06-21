043a6ef384111c39f2e92fb618d5f342407e8b72
Shi Hao Hong <shihaohong94@gmail.com>
2023-06-21 10:26:02 +0800

    ls - take argument for directory path

--- prep_phase/learning_c/ls/ls.c
+++ prep_phase/learning_c/ls/ls.c
@@ -2,19 +2,26 @@
 #include <sys/types.h>
 #include <dirent.h>
 
+#define MAX_PATH 256
+
 // See https://www.gnu.org/software/libc/manual/html_node/Accessing-Directories.html
 // for the directory access APIs.
 
 // TODO: error handling. exit 0 on success, >0 with errors
-int main() {
+int main(int argc, char *argv[]) {
     DIR *dp;
     struct dirent *ep;
 
+    // hard-code for first argument, ignore options for now
+    // TODO: implement taking options
+    char* path = argv[1];
+    if (path == NULL) {
+        path = "./";
+    }
+
     // opens a directory stream
-    // TODO: take argument for dir to read instead of hard-coded current dir
-    // handle both relative path and absolute path
     // TODO: filter results by hidden files (ignore files that start with ".")
-    dp = opendir("./");
+    dp = opendir(path);
     if (dp != NULL) {
         // reads directory stream until NULL when error/end
         while ((ep = readdir(dp))) {
