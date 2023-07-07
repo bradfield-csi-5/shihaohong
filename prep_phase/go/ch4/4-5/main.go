1a0a31e1cad29df7b97040952f9f5872b60e1141
Shi Hao Hong <shihaohong94@gmail.com>
2023-07-07 09:51:37 +0800

    comments and optimizations

--- prep_phase/go/ch4/4-5/main.go
+++ prep_phase/go/ch4/4-5/main.go
@@ -4,20 +4,25 @@ import "fmt"
 
 func main() {
 	s := []string{"storm", "storm", "storm", "earth", "earth", "fire", "heed", "my", "my", "my", "my", "call"}
-	fmt.Println(s)
+	fmt.Println(s) // [storm storm storm earth earth fire heed my my my my call]
 	s = removeDuplicates(s[:])
-	fmt.Println(s)
+	fmt.Println(s) // [storm earth fire heed my call]
+
+	s = []string{"storm", "earth", "fire", "heed", "my", "call"}
+	fmt.Println(s) // [storm earth fire heed my call]
+	s = removeDuplicates(s[:])
+	fmt.Println(s) // [storm earth fire heed my call]
 }
 
 func removeDuplicates(x []string) []string {
 	for i := 0; i < len(x)-1; i++ {
-		numDuplicates := 0
 		j := i + 1
 		c1 := x[i]
 		c2 := x[j]
 
 		// advance until non-duplicate
 		if c1 == c2 {
+			numDuplicates := 0
 			for c1 == c2 {
 				numDuplicates++
 				j++
@@ -30,9 +35,8 @@ func removeDuplicates(x []string) []string {
 				j++
 				k++
 			}
+			x = x[:len(x)-numDuplicates]
 		}
-
-		x = x[:len(x)-numDuplicates]
 	}
 
 	return x
