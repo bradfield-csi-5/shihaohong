43b81018f70544e96c4653551411d04758292b67
Shi Hao Hong <shihaohong94@gmail.com>
2023-07-19 12:14:57 +0800

    7-1 alternative soln

--- prep_phase/go/ch7/7-1/main.go
+++ prep_phase/go/ch7/7-1/main.go
@@ -6,58 +6,76 @@ import (
 	"strings"
 )
 
-type Counter struct {
-	count     int
-	splitFunc bufio.SplitFunc
+type LineCounter int
+type WordCounter int
+
+func (wc *WordCounter) Write(p []byte) (int, error) {
+	s := bufio.NewScanner(strings.NewReader(string(p)))
+	count, err := countUsingSplit(s, bufio.ScanWords)
+	if err != nil {
+		return 0, err
+	}
+
+	*wc += WordCounter(count)
+	return len(p), nil
 }
 
-func (c *Counter) Write(p []byte) (int, error) {
+func (lc *LineCounter) Write(p []byte) (int, error) {
 	s := bufio.NewScanner(strings.NewReader(string(p)))
-	s.Split(c.splitFunc)
+	count, err := countUsingSplit(s, bufio.ScanLines)
+	if err != nil {
+		return 0, err
+	}
 
+	*lc += LineCounter(count)
+	return len(p), nil
+}
+
+func countUsingSplit(s *bufio.Scanner, sf bufio.SplitFunc) (int, error) {
+	s.Split(sf)
+
+	count := 0
 	for s.Scan() {
-		c.count++
+		count++
 	}
 
 	if err := s.Err(); err != nil {
 		return 0, err
 	}
 
-	return len(p), nil
+	return count, nil
 }
 
 func main() {
 	// test WordCounter
 	str := "lorem ipsum text blah blah blah   hahaha"
-	var wc = Counter{
-		splitFunc: bufio.ScanWords,
-	}
+	var wc WordCounter
 
+	fmt.Println("===WordCounter===")
 	fmt.Fprintf(&wc, "input string: %s", str)
-	fmt.Printf("word count: %d\n", wc.count)
+	fmt.Printf("word count: %d\n", wc)
 	fmt.Fprintf(&wc, "more strings")
-	fmt.Printf("word count: %d\n", wc.count)
+	fmt.Printf("word count: %d\n", wc)
 	res, err := wc.Write([]byte("interesting content here"))
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("num bytes read: %d\n", res)
-	fmt.Printf("word count: %d\n", wc.count)
+	fmt.Printf("word count: %d\n", wc)
 
 	// test LineCounter
 	str = "lorem ipsum text \nblah blah blah   hahaha"
-	var lc = Counter{
-		splitFunc: bufio.ScanLines,
-	}
+	var lc LineCounter
 
+	fmt.Println("===LineCounter===")
 	fmt.Fprintf(&lc, "input string: %s", str)
-	fmt.Printf("line count: %d\n", lc.count)
+	fmt.Printf("line count: %d\n", lc)
 	fmt.Fprintf(&lc, "more strings")
-	fmt.Printf("line count: %d\n", lc.count)
+	fmt.Printf("line count: %d\n", lc)
 	res, err = lc.Write([]byte("interesting \ncontent \nhere"))
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("num bytes read: %d\n", res)
-	fmt.Printf("line count: %d\n", lc.count)
+	fmt.Printf("line count: %d\n", lc)
 }
