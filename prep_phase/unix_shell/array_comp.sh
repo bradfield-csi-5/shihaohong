9830cee16bea3e9c3a823684edf8525d1efebe9f
Shi Hao Hong <shihaohong94@gmail.com>
2023-07-29 18:08:42 +0800

    learnshell.org: array_comp.sh

---
+++ prep_phase/unix_shell/array_comp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# In this exercise, you will need to compare three list of arrays and write the common elements of all the three arrays:
+a=(3 5 8 10 6)
+b=(6 5 4 12)
+c=(14 7 5 7)
+
+for i in "${a[@]}"
+do
+    for j in "${b[@]}"
+    do
+        for k in "${c[@]}"
+        do
+            if [[ i -eq j && j -eq k ]] ; then
+                echo "$i"
+            fi
+        done
+    done
+done
