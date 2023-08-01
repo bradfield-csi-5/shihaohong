aa57c01775bd93395b8535228b1d79cdff05c982
Shi Hao Hong <shihaohong94@gmail.com>
2023-08-01 16:33:44 +0800

    Fix wiki.sh to use WikiMedia APIs for fine-tuned requests

--- prep_phase/unix_shell/wiki/wiki.sh
+++ prep_phase/unix_shell/wiki/wiki.sh
@@ -1,46 +1,21 @@
 #!/bin/bash
 
 TMP_FILENAME="out.txt"
+WIKIPEDIA_API_URL="https://en.wikipedia.org/w/api.php"
 
 remove_html_tags() {
     # sed to search for all <*> and remove them
     echo "$1" | sed 's/<[^>]*>//g'
 }
 
-print_title_article() {
-    # grep to search for the main title
-    search_result=$(grep '<span class="mw-page-title-main">.*</span>' "$1")
-    # echo title result
-    # echo $search_result
-
-    filtered_result=$(remove_html_tags "$search_result")
-
-    # trim whitespaces
-    read -r filtered_result <<< "$filtered_result"
-    # echo filtered result
-    echo "$filtered_result"
-}
-
 print_first_sentence_article() {
-    # Assume the first <p> followed by no other tags is the first sentence of the paragraph
-    # TODO(shihaohong): this filter doesn't actually work all the time. Examples:
-    # - <p><b> Malaysia ...
-    # - <p> tags in tables preceding the first sentence, usually on the right side of the wiki article
-    # idea: loosely look for <b> within a <p> tag? that's usually the first sentence highlights the search term
-    search_result=$(grep '<p>[a-zA-Z].*[.!?] ' "$1")
+    curl "$WIKIPEDIA_API_URL?format=json&action=query&prop=extracts&exsentences=1&explaintext&titles=$1" -s > section.txt
+    search_result=$(grep -o '"extract":".*"' section.txt | awk -F'"' '{print $4}')
     # echo searched results
     # echo "${search_result:0:1000}"
-    filtered_result=$(remove_html_tags "$search_result")
-
-    # echo filtered results
-    # echo "${filtered_result:0:1000}"
-    substring=". "
-
-    search=${filtered_result%%"$substring"*}
-    search_idx=$((${#search} + 1))
 
     # echo filtered sentence
-    echo "${filtered_result:0:search_idx}"
+    echo "$search_result"
 }
 
 print_section_headings_article() {
@@ -61,9 +36,9 @@ fi
     # regex and grab the list of section headings
 if  [ -z "$2" ]; then
     echo "---Title of Wikipedia Article:---"
-    print_title_article "$TMP_FILENAME"
+    echo "$1"
     echo "---First sentence:---"
-    print_first_sentence_article "$TMP_FILENAME"
+    print_first_sentence_article "$1"
     echo "---List of Sections:---"
     print_section_headings_article "$TMP_FILENAME"
 # if third argument:
