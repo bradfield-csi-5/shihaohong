830548b127f0e78dad97188f0b0f20cdc188a7c5
Shi Hao Hong <shihaohong94@gmail.com>
2023-08-13 14:22:10 +0800

    complete sum_to_n

--- m1_computer_systems/assembly-prework/sum_to_n.asm
+++ m1_computer_systems/assembly-prework/sum_to_n.asm
@@ -1,4 +1,14 @@
 section .text
 global sum_to_n
-sum_to_n:
+sum_to_n:			; rdi is the input val
+	xor rbx, rbx 	; reserve a register for total, set to zero
+	xor r8, r8 		; set register for iterator
+
+accumulate:
+	add rbx, r8
+	inc r8
+	cmp r8, rdi
+	jle accumulate
+
+	mov rax, rbx 	; save total to rax
 	ret
