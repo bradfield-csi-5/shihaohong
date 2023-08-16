93c2baa6b413455b2f7f7222b86a9d87262aa607
Shi Hao Hong <shihaohong94@gmail.com>
2023-08-16 11:14:02 +0800

    complete fib.asm

--- m1_computer_systems/assembly/assembly-2-prework/fib.asm
+++ m1_computer_systems/assembly/assembly-2-prework/fib.asm
@@ -1,4 +1,19 @@
 section .text
 global fib
 fib:
+	cmp rdi, 1
+	jg recurse
+	mov rax, rdi
+	ret
+
+recurse:
+	dec rdi
+	push rdi
+	call fib
+	pop rdi
+	push rax
+	dec rdi
+	call fib
+	pop rdi
+	add rax, rdi
 	ret
