8cf9869209f5850aeba207e631d7906839f73c36
Shi Hao Hong <shihaohong94@gmail.com>
2023-08-16 11:58:44 +0800

    complete cone.asm

--- m1_computer_systems/assembly/assembly-2-prework/cone.asm
+++ m1_computer_systems/assembly/assembly-2-prework/cone.asm
@@ -3,4 +3,13 @@ default rel
 section .text
 global volume
 volume:
+	mulss xmm0, xmm0 ; r * r
+	mulss xmm0, xmm1 ; r^2 * h
+	mulss xmm0, [pi] ; pi * r^2 * h
+	cvtsi2ss xmm1, [const_3]
+	divss xmm0, xmm1 ; pi * r^2 * h / 3
  	ret
+
+			section 	.data
+pi:			dd 			3.14159265359
+const_3:	dd			3
