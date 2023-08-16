1c53994165fc10b65a4b13de679f10be2e2092d0
Shi Hao Hong <shihaohong94@gmail.com>
2023-08-16 11:39:29 +0800

    complete matrix.asm

--- m1_computer_systems/assembly/assembly-2-prework/matrix.asm
+++ m1_computer_systems/assembly/assembly-2-prework/matrix.asm
@@ -6,4 +6,13 @@ index:
 	; rdx: cols
 	; rcx: rindex
 	; r8: cindex
+
+	; calculate the number of rows to jump
+	; need to determine size of row
+	mov r9, 1
+	imul r9, rcx
+	imul r9, rdx
+	lea rdi, [rdi + 4 * r9] ; handle row jump
+	mov rax, [rdi + 4 * r8] ; handle column jump
+
 	ret
