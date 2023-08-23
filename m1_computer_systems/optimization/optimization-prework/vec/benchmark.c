f4987fc7009b422885768b0fd37a258c7d2af89f
Shi Hao Hong <shihaohong94@gmail.com>
2023-08-23 11:22:51 +0800

    add benchmarks

---
+++ m1_computer_systems/optimization/optimization-prework/vec/benchmark.c
@@ -0,0 +1,43 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "dotproduct.c"
+
+void benchmark(long n, data_t (*f)(vec_ptr, vec_ptr), char* func_name) {
+  clock_t baseline_start, baseline_end, test_start, test_end;
+  double clocks_elapsed, time_elapsed;
+  baseline_start = clock();
+  int i = 0;
+  vec_ptr u = new_vec(n);
+  vec_ptr v = new_vec(n);
+
+  for (long i = 0; i < n; i++) {
+    set_vec_element(u, i, i + 1);
+    set_vec_element(v, i, i + 1);
+  }
+
+  for (i = 0; i < n; i++) {
+  }
+  baseline_end = clock();
+
+  test_start = clock();
+  for (i = 0; i < n; i++) {
+    (*f)(u, v);
+  }
+  test_end = clock();
+
+  clocks_elapsed = test_end - test_start - (baseline_end - baseline_start);
+  time_elapsed = clocks_elapsed / CLOCKS_PER_SEC;
+
+  printf("func: %s -> %.2fs to run %ld tests (%.2fns per test)\n", func_name, time_elapsed, n,
+         time_elapsed * 1e9 / n);
+}
+
+// sample use
+int main() {
+    benchmark(20000, dotproduct, "dotproduct_raw");
+    benchmark(20000, dotproduct_reduce_proc_call, "dotproduct_reduce_proc_call");
+    benchmark(20000, dotproduct_unrolled_2_1, "dotproduct_unrolled_2_1");
+    benchmark(20000, dotproduct_unrolled_2_2, "dotproduct_unrolled_2_2");
+}
--- m1_computer_systems/optimization/optimization-prework/vec/dotproduct.c
+++ m1_computer_systems/optimization/optimization-prework/vec/dotproduct.c
@@ -1,25 +1,71 @@
 #include "vec.h"
 
-
 data_t dotproduct(vec_ptr u, vec_ptr v) {
-   data_t sum1 = 0, sum2 = 0, u_val, v_val;
+   data_t sum = 0, u_val, v_val;
+   for (long i = 0; i < vec_length(u); i++) { // we can assume both vectors are same length
+      get_vec_element(u, i, &u_val);
+      get_vec_element(v, i, &v_val);
+      sum += u_val * v_val;
+   }
+
+   return sum;
+}
+
+data_t dotproduct_reduce_proc_call(vec_ptr u, vec_ptr v) {
+   data_t sum = 0, u_val, v_val;
+   long len = vec_length(u);
+   for (long i = 0; i < len; i++) { // we can assume both vectors are same length
+      get_vec_element(u, i, &u_val);
+      get_vec_element(v, i, &v_val);
+      sum += u_val * v_val;
+   }
+
+   return sum;
+}
+
+data_t dotproduct_unrolled_2_1(vec_ptr u, vec_ptr v) {
+   data_t sum = 0, u_val, v_val;
    long len = vec_length(u);
    long limit = len-1;
    long i = 0;
    for (; i < limit; i += 2) { // we can assume both vectors are same length
       get_vec_element(u, i, &u_val);
       get_vec_element(v, i, &v_val);
-      sum1 += u_val * v_val;
+      sum += u_val * v_val;
 
       get_vec_element(u, i + 1, &u_val);
       get_vec_element(v, i + 1, &v_val);
-      sum2 += u_val * v_val;
+      sum += u_val * v_val;
    }
 
    for (; i < len ; i++ ) {
       get_vec_element(u, i, &u_val);
       get_vec_element(v, i, &v_val);
-      sum1 += u_val * v_val;
+      sum += u_val * v_val;
+   }
+
+   return sum;
+}
+
+data_t dotproduct_unrolled_2_2(vec_ptr u, vec_ptr v) {
+   data_t sum1 = 0, sum2 = 0, u_val1, v_val1, u_val2, v_val2;
+   long len = vec_length(u);
+   long limit = len-1;
+   long i = 0;
+   for (; i < limit; i += 2) { // we can assume both vectors are same length
+      get_vec_element(u, i, &u_val1);
+      get_vec_element(v, i, &v_val1);
+      sum1 += u_val1 * v_val1;
+
+      get_vec_element(u, i + 1, &u_val2);
+      get_vec_element(v, i + 1, &v_val2);
+      sum2 += u_val2 * v_val2;
+   }
+
+   for (; i < len ; i++ ) {
+      get_vec_element(u, i, &u_val1);
+      get_vec_element(v, i, &v_val1);
+      sum1 += u_val1 * v_val1;
    }
 
    return sum1 + sum2;
