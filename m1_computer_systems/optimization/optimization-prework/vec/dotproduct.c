25eac631b67eb6ad39960bbe5d49c8770c001db5
Shi Hao Hong <shihaohong94@gmail.com>
2023-08-23 10:52:57 +0800

    2 x 1 loop unrolling

--- m1_computer_systems/optimization/optimization-prework/vec/dotproduct.c
+++ m1_computer_systems/optimization/optimization-prework/vec/dotproduct.c
@@ -4,11 +4,23 @@
 data_t dotproduct(vec_ptr u, vec_ptr v) {
    data_t sum = 0, u_val, v_val;
    long len = vec_length(u);
+   long limit = len-1;
+   long i = 0;
+   for (; i < limit; i += 2) { // we can assume both vectors are same length
+      get_vec_element(u, i, &u_val);
+      get_vec_element(v, i, &v_val);
+      sum += u_val * v_val;
 
-   for (long i = 0; i < len; i++) { // we can assume both vectors are same length
-        get_vec_element(u, i, &u_val);
-        get_vec_element(v, i, &v_val);
-        sum += u_val * v_val;
+      get_vec_element(u, i + 1, &u_val);
+      get_vec_element(v, i + 1, &v_val);
+      sum += u_val * v_val;
    }
+
+   for (; i < len ; i++ ) {
+      get_vec_element(u, i, &u_val);
+      get_vec_element(v, i, &v_val);
+      sum += u_val * v_val;
+   }
+
    return sum;
 }
