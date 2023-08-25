6fff28fa26a16bd3b9dda15cee005798eb9a8fab
Shi Hao Hong <shihaohong94@gmail.com>
2023-08-25 15:15:09 +0800

    attempt matrix mult, more notes for loop-order

--- m1_computer_systems/memory_hierarchy/memory-hierarchy-prework/loop-order.c
+++ m1_computer_systems/memory_hierarchy/memory-hierarchy-prework/loop-order.c
@@ -16,15 +16,24 @@ but due to spatial locality, option_one should run
 faster due to its cache utilization.
 
 On Apple M1 Pro:
+
+<-- THESE ARE THE SMALLER CORES -->
 memory-hierarchy-prework shihao$ sysctl -a | grep hw.l
 hw.l1icachesize: 131072
 hw.l1dcachesize: 65536
 hw.l2cachesize: 4194304
 
+<-- LOOKING AT WIKIPEDIA -->
+L1 cache of high-performance cores:
+l1i: 192KB
+l1d: 128KB
+L2 cache of high-performance cores:
+shared 12MB
+
+L1 cache of efficient cores:
 l1d stores data, l1i stores instructions
 l1d: ~65.5KB
 l2: ~4.2MB
-l3: 8MB (shared for all clusters)
 
 l1 cache latency: 3-4 cycles
 l2 cache latency: 18 cycles
@@ -35,16 +44,24 @@ matrix size: ~64MB
 row size: 16KB
 
 Option 1:
+assuming efficient core:
 l1 cache is reset at least ~1000 times
 l2 cache is reset at least 16 times
 
-Option 2:
-l1 cache can store about 4 rows, so it'll be utilized every 4 reads.
-l1 cache is reset at least 4,000,000 times
-l2 cache can store about ~260 rows, so it'll be utilized every 260 reads
-l2 cache is reset at least 61,538 times
+assuming high-perf core:
+l1 cache is reset at least ~500 times
+l2 cache is reset at least 5 times
 
-Difference in clock cycles:
+Option 2:
+assuming efficient core:
+l1 cache is reset at least 4,000,000 times
+l2 cache is reset at least ~60k times
+
+assuming high-perf core:
+l1 cache is reset at least 2,000,000 times
+l2 cache is reset at least ~30k times
+
+Difference in clock cycles (using worst case):
 4M * 3-4 = 12M cycles
 61K * 18 = 1M cycles
 13M clock cycles
@@ -58,14 +75,30 @@ Using benchmarking code, can see it's about an order of magnitude slower than pr
 Time taken (option_one): 0.028s
 Time taken (option_one): 0.083s
 Diff: 0.055s = 55ms
+
+Instrument results:
+-- Random observation, the work is split across multiple cores
+
+Option 1:
+L1d miss, L2 miss, elements
+212k, 26k, one element
+339k, 78k, 4kx4k elements
+DIFF
+127k, 52k
+
+Option 2:
+176k, 22k, one element
+200k, 14.7M, 4kx4k elements
+DIFF
+24k, ~14M
 */
 
-int x[4000][4000];
+int x[1][1];
 
 void option_one() {
   int i, j;
-  for (i = 0; i < 4000; i++) {
-    for (j = 0; j < 4000; j++) {
+  for (i = 0; i < 1; i++) {
+    for (j = 0; j < 1; j++) {
       x[i][j] = i + j;
     }
   }
@@ -73,17 +106,17 @@ void option_one() {
 
 void option_two() {
   int i, j;
-  for (i = 0; i < 4000; i++) {
-    for (j = 0; j < 4000; j++) {
+  for (i = 0; i < 1; i++) {
+    for (j = 0; j < 1; j++) {
       x[j][i] = i + j;
     }
   }
 }
 
 int main() {
-  option_one();
-  // option_two();
-  int v = x[1][1];
+  // option_one();
+  option_two();
+  // int v = x[0][0];
 
   // clock_t start, stop;
   // double time;
--- m1_computer_systems/memory_hierarchy/memory-hierarchy-prework/matrix-multiply.c
+++ m1_computer_systems/memory_hierarchy/memory-hierarchy-prework/matrix-multiply.c
@@ -7,6 +7,9 @@ There must be a better way!
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TILE_SIZE 32
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+
 /*
   A naive implementation of matrix multiplication.
 
@@ -14,9 +17,12 @@ There must be a better way!
   currently does
 */
 void matrix_multiply(double **C, double **A, double **B, int a_rows, int a_cols,
-                     int b_cols) {
-  for (int i = 0; i < a_rows; i++) {
-    for (int j = 0; j < b_cols; j++) {
+                     int b_cols)
+{
+  for (int i = 0; i < a_rows; i++)
+  {
+    for (int j = 0; j < b_cols; j++)
+    {
       C[i][j] = 0;
       for (int k = 0; k < a_cols; k++)
         C[i][j] += A[i][k] * B[k][j];
@@ -24,8 +30,27 @@ void matrix_multiply(double **C, double **A, double **B, int a_rows, int a_cols,
   }
 }
 
-void fast_matrix_multiply(double **c, double **a, double **b, int a_rows,
+void fast_matrix_multiply(double **C, double **A, double **B, int a_rows,
                           int a_cols, int b_cols) {
-  // TODO: write a faster implementation here!
-  return matrix_multiply(c, a, b, a_rows, a_cols, b_cols);
+  int ti, tj, tk,          // indexes of the tile
+      i, j, k,             // indexes within a tile
+      i_end, j_end, k_end; // end when matrix dim not a multiple of tile_size
+
+  for (ti = 0; ti < a_rows; ti += TILE_SIZE) {
+    i_end = min(ti + TILE_SIZE, a_rows);
+
+    for (tj = 0; tj < b_cols; tj += TILE_SIZE / 2) {
+      j_end = min(tj + TILE_SIZE / 2, b_cols);
+
+      for (tk = 0; tk < a_cols; tk += TILE_SIZE) {
+        k_end = min(tk + TILE_SIZE, a_cols);
+
+        // Compute this tile
+        for (i = ti; i < i_end; i++)
+          for (j = tj; j < j_end; j++)
+            for (k = tk; k < k_end; k++)
+              C[i][j] += A[i][k] * B[k][j];
+      }
+    }
+  }
 }
