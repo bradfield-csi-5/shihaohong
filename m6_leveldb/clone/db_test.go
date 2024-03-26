680a36ccd0cb4a66146b0ce4d5184491d41ce55e
Shi Hao Hong <shihaohong94@gmail.com>
2024-03-26 11:02:12 +0800

    tentative rangescan benchmark

--- m6_leveldb/clone/db_test.go
+++ m6_leveldb/clone/db_test.go
@@ -46,4 +46,26 @@ func BenchmarkMemoryDBDelete(b *testing.B) {
 }
 
 func BenchmarkMemoryDBRangeScan(b *testing.B) {
+	alphabet := []string{}
+	for i := 'A'; i <= 'Z'; i++ {
+		alphabet = append(alphabet, string(i))
+	}
+
+	db := NewMemoryDB()
+	for i := 0; i < len(alphabet); i++ {
+		for j := 0; j < 100; j++ {
+			key := []byte(alphabet[i] + fmt.Sprint(j))
+			val := []byte("item" + alphabet[i] + fmt.Sprint(j))
+			db.Put(key, val)
+		}
+	}
+	b.ResetTimer()
+	iter, _ := db.RangeScan([]byte("A50"), []byte("B50"))
+
+	// get all iterator values until exhausted
+	for iter.Next() {
+		// just to verify results of rangescan
+		// fmt.Println(string(iter.Key()))
+		// fmt.Println(string(iter.Value()))
+	}
 }
